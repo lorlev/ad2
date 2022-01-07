37863a18cb9d2a8a1d24578cbfc0b1276caea34f
Levani <levani.ramazashvili90@gmail.com>
2022-01-07 13:41:36 +0400

    Update functions.sh

--- inc/functions.sh
+++ inc/functions.sh
@@ -15,10 +15,19 @@ LoadEnv(){
 SelfUpdate(){
 	cd $local_path
 
-	if [[ `git status --porcelain` ]]; then
-		echo 'Changes'
+	UPSTREAM=${1:-'@{u}'}
+	LOCAL=$(git rev-parse @)
+	REMOTE=$(git rev-parse "$UPSTREAM")
+	BASE=$(git merge-base @ "$UPSTREAM")
+
+	if [ $LOCAL = $REMOTE ]; then
+		echo "Up-to-date"
+	elif [ $LOCAL = $BASE ]; then
+		echo "Need to pull"
+	elif [ $REMOTE = $BASE ]; then
+		echo "Need to push"
 	else
-		echo 'No changes'
+		echo "Diverged"
 	fi
 }
 
